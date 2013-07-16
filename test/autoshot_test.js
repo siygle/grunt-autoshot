9d22f31b57086b7dff269f4340a84f60e239f949
Ferrari <shiyung@gmail.com>
2013-07-17 03:02:02 +0800

    remove yo test case first

--- test/autoshot_test.js
+++ test/autoshot_test.js
@@ -28,20 +28,7 @@ exports.autoshot = {
     done();
   },
   default_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
-
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
-
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    //TODO
 
     test.done();
   },
